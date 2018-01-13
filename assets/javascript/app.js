76f7f3dfad0481d3cf9e3a5c6ef36c59f2cdc977
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2018-01-13 12:52:31 -0600

    Nothing too important

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,14 @@
-// Test comment
+// Initialize Firebase
+
+var config = {
+apiKey: "AIzaSyDPHQ_orUz5fD6OZ4j55o0HzaLg9GyeJCc",
+authDomain: "tutor-149b6.firebaseapp.com",
+databaseURL: "https://tutor-149b6.firebaseio.com",
+projectId: "tutor-149b6",
+storageBucket: "tutor-149b6.appspot.com",
+messagingSenderId: "637595601297"
+};
+
+firebase.initializeApp(config);
+
+// Second test, second test
